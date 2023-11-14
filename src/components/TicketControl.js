e4c34ca9063e66259c411a7c8cebfb454dec5eec
Richard Barbour <richyb07@gmail.com>
2023-11-13 17:51:04 -0800

    Add toggle functionality between TicketList and NewTicketForm.

--- src/components/TicketControl.js
+++ src/components/TicketControl.js
@@ -6,12 +6,31 @@ class TicketControl extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      formVisibleOnPage: false
+    };
+  }
+
+  handleClick = () => {
+    this.setState(prevState => ({
+      formVisibleOnPage: !prevState.formVisibleOnPage
+    }));
   }
 
   render(){
+    let currentlyVisibleState = null;
+    let buttonText = null;
+    if (this.state.formVisibleOnPage) {
+      currentlyVisibleState = <NewTicketForm />
+      buttonText = "Return to Ticket List";
+    } else {
+      currentlyVisibleState = <TicketList />
+      buttonText = "Add Ticket";
+    }
     return (
       <React.Fragment>
+        {currentlyVisibleState}
+        <button onClick={this.handleClick}>{buttonText}</button>
       </React.Fragment>
     );
   }
