ed0d8a7ce2add9e549c62ccc717b0602cb4e4fea
Richard Barbour <richyb07@gmail.com>
2023-11-27 11:35:35 -0800

    Polish ticket-list-reducer.js

--- src/reducers/ticket-list-reducer.js
+++ src/reducers/ticket-list-reducer.js
@@ -1,21 +1,21 @@
 const reducer = (state = {}, action) => {
   const { names, location, issue, id } = action;
   switch (action.type) {
-  case 'ADD_TICKET':
-    return Object.assign({}, state, {
-      [id]: {
-        names: names,
-        location: location,
-        issue: issue,
-        id: id
-      }
-    });
+    case 'ADD_TICKET':
+      return Object.assign({}, state, {
+        [id]: {
+          names: names,
+          location: location,
+          issue: issue,
+          id: id
+        }
+      });
     case 'DELETE_TICKET':
       let newState = { ...state };
       delete newState[id];
       return newState;
-  default:
-    return state;
+    default:
+      return state;
   }
 };
 
